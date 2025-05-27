09413095370129338a40e77a47176c6388c237a2
Pacpac4577 <maellie.colombwt@gmail.com>
2025-05-27 11:27:35 +0200

    Ajout sketch 6

--- sketch 6.js
+++ sketch 6.js
@@ -1,9 +1,27 @@
 function setup() {
-    createCanvas(800, 800);
+  createCanvas(800, 800);
+  background(220);
+
+  for (let y = 0; y < 15; y++) {
+    for (let x = 0; x < 15; x++) {
+      noStroke();
+      print(y, x);
+      fill(x * 64, 50, y * 200);
+      square(x * 60, y * 60, 60);
+    }
   }
-  
-  function draw() {
-    background(107, 135, 228);
-    circle( 200,200,100 );
-    square ( 200,200,50);
+}
+
+function draw() {
+  for (let y = 0; y < 15; y++) {
+    for (let x = 0; x < 15; x++) {
+      let r = (x * 64 + frameCount *15) % 256; 
+      //frameCount comme des images de pelicule film// *15  est egale a la vitesse 
+      
+      let b = (y * 64 + frameCount *3) % 256;
+
+      fill(r, 50, b, 100); 
+      square(x * 50, y * 50, 50);
+    }
   }
+}
