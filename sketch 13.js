818fc1d68576bf8c27f0426a0fe60f3e79e75421
Pacpac4577 <maellie.colombwt@gmail.com>
2025-05-29 14:40:30 +0200

    Ajout sketch 13

--- sketch 13.js
+++ sketch 13.js
@@ -1,9 +1,33 @@
+let myImage;
+
+function preload() {
+  myImage = loadImage("img/P2140023.jpg");
+}
+
 function setup() {
-    createCanvas(800, 800);
+  createCanvas(800, 400);
+  background(255);
+  myImage.resize(400, 300);
+
+  image(myImage, 0, 0);
+
+  myImage.loadPixels();
+
+  for (let i = 0; i < myImage.pixels.length; i += 4) {
+    
+    myImage.pixels[i] += 80;
+    myImage.pixels[i + 1] += 10;
+    myImage.pixels[i + 2] -= 10;
   }
+
+  myImage.updatePixels();
+
+  image(myImage, myImage.width, 0);
   
-  function draw() {
-    background(107, 135, 228);
-    circle( 200,200,100 );
-    square ( 200,200,50);
-  }
+
+  fill(255, 100, 180);
+  textSize(24);
+  text("Filtre cute 💕", 10, height - 10);
+}
+
+function draw() {}
