4853a223491f253d08ea264c08052007d0923e8f
Pacpac4577 <maellie.colombwt@gmail.com>
2025-05-29 14:55:39 +0200

    Ajout sketch 15

--- sketch 15.js
+++ sketch 15.js
@@ -1,9 +1,44 @@
+let boxSize = 20;
+
+let posX = 50;
+let posY = 50;
+
+let vitX = 2;
+let vitY = 1.234;
+
+
 function setup() {
-    createCanvas(800, 800);
+  createCanvas(200, 200);
+}
+
+function draw() {
+   background(220, 20);
+  bouger();
+  rebondir();
+  afficher();
+}
+
+
+function afficher(){
+  fill(255);
+  square(posX, posY, boxSize);
+  fill(255,0,255,255);
+  ellipseMode(CORNER);
+  circle(posX, posY, boxSize)
+  fill(255);
+  circle(posX + 5, posY + 5, boxSize - 10);
+}
+
+function bouger(){
+  posX = posX + vitX;
+  posY += vitY; 
+}
+
+function rebondir(){
+  if( posX + boxSize > width || posX < 0 ){
+    vitX = vitX * -1;
   }
-  
-  function draw() {
-    background(107, 135, 228);
-    circle( 200,200,100 );
-    square ( 200,200,50);
+  if( posY + boxSize > height || posY < 0 ){
+    vitY *= -1;
   }
+}
