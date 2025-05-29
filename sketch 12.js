d53c98c926ede216fe4fc92ced820faae8c5d473
Pacpac4577 <maellie.colombwt@gmail.com>
2025-05-29 14:25:26 +0200

    Ajout du sketch 12

--- sketch 12.js
+++ sketch 12.js
@@ -1,9 +1,22 @@
 function setup() {
-    createCanvas(800, 800);
+  createCanvas(400, 400);
+  background(255, 200, 220);
+
+  let cercles = [40, 8, 15, 16, 23, 42, 80, 33, 24, 50, 25];
+
+  fill(255, 100, 180);
+  noStroke();
+
+  for (let i = 0; i < cercles.length; i++) {
+    let x = (i % 6) * 60 + 30;
+    let y = floor(i / 6) * 100 + 150;
+
+    circle(x, y, cercles[i]);
   }
-  
-  function draw() {
-    background(107, 135, 228);
-    circle( 200,200,100 );
-    square ( 200,200,50);
-  }
+
+  fill(255);
+  textSize(16);
+  text("Des cercles qui se baladent 💗", 10, 30);
+}
+
+function draw() {}
