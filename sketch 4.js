9b6c628a6efb2e20920a4bc581bd9cfc7ac107b3
Pacpac4577 <maellie.colombwt@gmail.com>
2025-05-29 13:05:53 +0200

    Ajout sketch 4
    dumb photoshop (mini modif)

--- sketch 4.js
+++ sketch 4.js
@@ -1,12 +1,42 @@
+let oldX = 0;
+let oldY = 0;
+
 function setup() {
-    createCanvas(800, 800);
-  }
+  createCanvas(windowWidth, windowHeight);
   
-  function draw() {
-    background(107, 135, 228);
-    circle( 200,200,100 );
-    square ( 200,200,50);
+  let R = random (255);
+  let V = random (255);
+  let B = random (255);
+  background(R,V,B) ;
+  noStroke();
+}
+
+function draw() {
+  //mouseX - oldX
+  
+  oldX = oldX + (mouseX - oldX) / 50;
+  oldY+= (mouseY- oldY) /50;
+  
+  circle(oldX,oldY,50);
+}
+
+function mouseClicked(){
+  let R = random (255);
+  let V = random (255);
+  let B = random (255);
+  
+  fill(R,V,B)
+}
+
+function keyPressed(){
+  if(key== "s"){
+  save("drawing.png")
   }
-
-
-  //A modifier !!!
+  if (key == "x"){
+    
+  let R = random (255);
+  let V = random (255);
+  let B = random (255);
+  background(R,V,B) ;
+  }
+}
