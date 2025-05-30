61e0352b3b033b402c05d9ecdb9344e628ffafb2
Pacpac4577 <maellie.colombwt@gmail.com>
2025-05-30 10:55:10 +0200

    16 update

--- sketch 16.js
+++ sketch 16.js
@@ -1,20 +1,18 @@
 function setup() {
-  function setup() {
-    createCanvas(200, 200);
-    noLoop();
-  }
-  
-  function draw() {
-    for (let x = 0; x < width; x += 20) {
-      for (let y = 0; y < height; y += 20) {
-        if ((x + y) % 40 == 0) {
-          fill(255, 200, 230);
-        } else {
-          fill(200, 240, 255);
-        }
-        noStroke();
-        rect(x, y, 20, 20);
+  createCanvas(200, 200);
+  noLoop();
+}
+
+function draw() {
+  for (let x = 0; x < width; x += 20) {
+    for (let y = 0; y < height; y += 20) {
+      if ((x + y) % 40 == 0) {
+        fill(255, 200, 230);
+      } else {
+        fill(200, 240, 255);
       }
+      noStroke();
+      rect(x, y, 20, 20);
     }
   }
 }
